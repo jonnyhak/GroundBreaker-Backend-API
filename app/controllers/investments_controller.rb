3d402ab056b0e3d3e7c2b0e14f880ed94f2589e7
jonnyhak <jonnyhak@gmail.com>
2021-01-14 12:04:35 -0500

    controllers

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,2 +1,3 @@
 class InvestmentsController < ApplicationController
+    
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,33 @@
 class ProjectsController < ApplicationController
+    def index
+        @projects = Project.all
+        render json: @projects
+    end
+
+    def show
+        @project = Project.find(params[:id])
+        render json: @project
+    end
+
+    def new
+        @project = Project.new
+        render json: @project
+    end
+
+    def create
+        @project = Player.create(project_params)
+        render json: @project
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        @project.update(project_params)
+        render json: @project
+    end
+
+    private
+
+    def project_params
+        params.require(:player).permit(:developer_name, :location, :total_capital_needed, :minimum_investment, :projected_roi)
+    end
 end
