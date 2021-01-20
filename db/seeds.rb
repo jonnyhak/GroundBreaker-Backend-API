27e2abff23fcf946ca2204d5f50ae5931a60be06
jonnyhak <jonnyhak@gmail.com>
2021-01-20 12:04:38 -0500

    re seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ jonnyhak = User.create(username: "jonnyhak", password: "password123")
 jallen = User.create(username: "jallen", password: "password123")
 sean = User.create(username: "sean", password: "password123")
 
-project1 = Project.create(developer_name: "project1", location: "East Village", total_capital_needed: 2400, minimum_investment: 500, projected_roi: 0.094)
-project2 = Project.create(developer_name: "project2", location: "Midtown", total_capital_needed: 18000, minimum_investment: 1500, projected_roi: 0.043)
-project3 = Project.create(developer_name: "project3", location: "Harlem", total_capital_needed: 36000, minimum_investment: 3000, projected_roi: 0.073)
+project1 = Project.create(developer_name: "project1", location: "East Village", total_capital_needed: 24000, minimum_investment: 500, projected_roi: 0.094)
+project2 = Project.create(developer_name: "project2", location: "Midtown", total_capital_needed: 180000, minimum_investment: 1500, projected_roi: 0.043)
+project3 = Project.create(developer_name: "project3", location: "Harlem", total_capital_needed: 360000, minimum_investment: 3000, projected_roi: 0.073)
 
 investment1 = Investment.create(date: Date.today, amount: 3300, user: jonnyhak, project: project3)
 investment2 = Investment.create(date: Date.today, amount: 2400, user: jonnyhak, project: project3)
